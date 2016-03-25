c6b5034a09a5f25ae7ccd904d137391b50dffe28
HeroicKatora <andreas.molzer@gmx.de>
2016-03-25 05:47:04 +0100

    Testing lxml stuff and moving away from module definition in xsd

--- src/gpp/__init__.py
+++ src/gpp/__init__.py
@@ -1,27 +1,37 @@
-'''
+"""
 Created on 13.03.2016
 
 @author: andreas
-'''
+"""
+from abc import ABCMeta, abstractclassmethod
+from lxml import etree
+from os import listdir
+
 from gpp.xmlparsing import XMLRegistry
 
 
-class PacketContent():
-    '''A part of a packet with dynamic size or content
-    '''
+class PacketContent:
+    """
+    A part of a packet with dynamic size or content
+    """
+    @abstractclassmethod
     def read(self, bitstream):
         pass
 
+    @abstractclassmethod
     def write(self, bitstream):
         pass
 
 
 class PacketField(PacketContent):
-    '''A field has additional printing and parsing capabilities
-    '''
+    """
+    A field has additional printing and parsing capabilities
+    """
+    @abstractclassmethod
     def print(self, charstream):
         pass
 
+    @abstractclassmethod
     def parse(self, charstream):
         pass
 
@@ -30,6 +40,28 @@ class PacketField(PacketContent):
 
 
 if __name__ == '__main__':
-    xmlReg = XMLRegistry()
-    xmlReg.addSchemaFile('./schemes/PacketSchema.xsd')
-    print(xmlReg.parser._namespace_map)
+    XS = "{http://www.w3.org/2001/XMLSchema}"
+    print('Testing XML validation')
+    with open('./template.xml') as template:
+        tempTree = etree.parse(template)
+    for file in filter(lambda p: p.endswith('.xsd'), listdir('./schemes')):
+        location = '/'.join(('.', 'schemes', file))
+        with open(location) as xsdFile:
+            xsdScheme = etree.parse(xsdFile)
+            namespace = xsdScheme.getroot().attrib['targetNamespace']
+            print(namespace, ' found at file ', location)
+            importXs = etree.Element(XS+'import')
+            importXs.attrib['namespace'] = namespace
+            importXs.attrib['schemaLocation'] = location
+            tempTree.getroot().append(importXs)
+    print(etree.tostring(tempTree, pretty_print=True))
+    templateSchema = etree.XMLSchema(tempTree)
+    parser = etree.XMLParser(schema=templateSchema, attribute_defaults=True, remove_blank_text=True, resolve_entities=True)
+    tree = etree.parse('./xml/StandardDocument.xml', parser)
+    print(etree.tostring(tree.getroot(), pretty_print=True))
+    print('Testing element int64')
+    int64 = tree.find('{http://github.com/HeroicKatora/PacketParsing}type')
+    if int64 is None:
+        print('Something went wrong')
+    else:
+        print(etree.tostring(int64))
--- src/gpp/xmlparsing/__init__.py
+++ src/gpp/xmlparsing/__init__.py
@@ -1,65 +1,81 @@
-'''
+"""
 Created on 13.03.2016
 
 @author: andreas
-'''
+"""
 
+from abc import ABCMeta, abstractclassmethod
 from enum import Enum
 from builtins import str, KeyError
-from xml.etree import ElementTree
-from gpp.xmlparsing import builtin
+from lxml import etree
+from . import builtin
 
 
 class SchemeLibrary(Enum):
     Builtin = builtin.library
 
 
-class PacketLibrary():
-    '''Library documentation class
-    '''
+class PacketLibrary(metaclass=ABCMeta):
+    """
+    Library documentation class
+    """
+    @abstractclassmethod
     def schemes(self):
-        '''Returns a collection of schemes
-        '''
+        """
+        :return: a collection of (schema, module) pairs added by the library
+        """
         pass
 
+    @abstractclassmethod
     def xml(self):
-        '''Returns a collection of xml files for the library
-        '''
+        """
+        :return: a collection of xml files contained in the library
+        """
         pass
 
 
 class XMLRegistry():
     def __init__(self):
-        self.parser = ElementTree
+        self.XS = "{http://www.w3.org/2001/XMLSchema}"
+        with open('./template.xml') as template:
+            self.tempTree = etree.parse(template)
+        self.parser = etree.ElementTree
         self.validationList = list()
         self.schemes = dict()
         self.addLibrary(SchemeLibrary.Builtin)
+        self.namepsace_implementors = dict()
 
-    def addSchemaFile(self, sourceURI):
-        parsedSchema = self.parser.parse(sourceURI)
-        nameSpace = parsedSchema.getroot().get('targetNamespace')
-        self.schemes.put(nameSpace, parsedSchema)
+    def add_schema_file(self, source_uri, implementing_module):
+        parsed_schema = self.parser.parse(source_uri)
+        xml_schema = etree.XMLSchema(parsed_schema)
+        namespace = parsed_schema.getroot().get('targetNamespace')
+        import_xs = etree.Element(self.XS+'import')
+        import_xs.attrib['namespace'] = namespace
+        import_xs.attrib['schemaLocation'] = source_uri
+        self.namepsace_implementors[namespace] = implementing_module
+        self.tempTree.getroot().append(import_xs)
 
-    def addInstanceFile(self, source):
-        parsedData = self.parser.parse(source)
-        self.validationList.append(parsedData)
-        return parsedData
+    def add_instance_file(self, source):
+        self.validationList.append(source)
 
-    def buildParser(self):
+    def build_parser(self):
         pass
 
-    def addLibrary(self, library):
-        if library.__class__ is str:
-            libCollect = SchemeLibrary(library)
-            for file in libCollect.schemes():
-                self.addSchemaFile(file)
-            for file in libCollect.xml():
-                self.addInstanceFile(file)
-            return
-        elif library.__class__ is SchemeLibrary:
-            for file in library.schemes():
-                self.addSchemaFile(file)
-            for file in library.xml():
-                self.addInstanceFile(file)
-            return
-        raise KeyError('Can\'t construct a library from the argument')
+    def add_library(self, library):
+        """
+        Use a string or a PacketLibrary object to add to the XMLRegistry
+        :param library: If a string, tries to resolves the corresponding SchemeLibrary Enum.
+            Else tries to add the object as a PacketLibrary
+        """
+        try:
+            if library.__class__ is str:
+                lib_collect = SchemeLibrary(library)
+                return self.add_library(lib_collect)
+            else:
+                for file, module in library.schemes():
+                    self.add_schema_file(file, module)
+                for file in library.xml():
+                    self.add_instance_file(file)
+                return
+        except Exception:
+            raise KeyError('Can\'t construct a library from the argument')

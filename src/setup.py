f4a4ee11e2174c8e6c2fe093faeececa67f29324
Andreas Molzer <andreas.molzer@gmx.de>
2016-09-15 19:13:47 +0200

    Add setup.py for installation

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+import os.path
+import setuptools
+
+setuptools.setup(
+    name='gpp',
+    version='0.1.0a1.dev1',
+    description='Parsing binary stream data with structures defined via XML and Python',
+    url='https://github.com/HeroicKatora/PacketParsing',
+    author='HeroicKatora',
+    license='MIT',
+    packages=setuptools.find_packages(where=os.path.dirname(__file__), exclude=['tests']),
+    install_requires=['lxml', 'bitstring', 'parse'],
+    package_data={'gpp': ['*.xml', '*.xsd']}
+)
